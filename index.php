8dcf9c15620c1d9de372b56d0d5acb3a30e8a82b
Paul Duffy <paulrduffy@gmail.com>
2015-06-19 11:09:11 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Simple PHP App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    </head>
+
+    <body bgcolor="#00FF00">
+        <div class="container">
+            <div class="hero-unit">
+                <h1><font face="verdana" color="yellow">Simple PHP Test Application EXTRA!</font></h1>
+                <br>
+                <br>
+                <p><font face="verdana" color="yellow" size="5">This simple PHP test application was deployed in a Runnable Sandbox!</font></p>
+                <br>
+                <br>
+                <p><font face="verdana" color="yellow" size="3">GitHub integration made this really simple to get running in a Runnable Sandbox</font></p>
+            </div>
+        </div>
+
+    </body>
+
+</html>
